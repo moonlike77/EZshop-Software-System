04c9219e333ca4cd998a9c1903cfee6e30090b74
Nima <s359242@studenti.polito.it>
2026-01-08 23:28:43 +0100

    Implemented Returns module with FastAPI and fixed tests structure

---
+++ app/models/DTO/return_dto.py
@@ -0,0 +1,19 @@
+from pydantic import BaseModel
+from typing import List, Optional
+from datetime import datetime
+from app.models.return_status import ReturnStatus
+
+class ReturnLineDTO(BaseModel):
+    id: Optional[int] = None
+    return_id: Optional[int] = None
+    product_barcode: str
+    quantity: int
+    price_per_unit: float
+
+class ReturnDTO(BaseModel):
+    id: int
+    sale_id: int
+    status: ReturnStatus
+    created_at: Optional[datetime] = None
+    closed_at: Optional[datetime] = None
+    lines: List[ReturnLineDTO] = []
--- main.py
+++ main.py
@@ -9,6 +9,8 @@ from app.routes import  user_route, auth_route
 from contextlib import asynccontextmanager
 from app.database.database import engine, Base
 from logging import getLogger
+from app.routes.return_route import return_bp
+from app.routes import user_route, auth_route, return_route  # <--- return_route
 
 logger = getLogger(__name__)
 
@@ -46,3 +48,9 @@ def read_root():
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
+
+# جایی که router ها رجیستر میشن:
+app.include_router(auth_route.router)
+app.include_router(user_route.router)
+
+app.include_router(return_bp)
