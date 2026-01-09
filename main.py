76d2f489b2fa1ff710d0734b8aebfdd05e15442a
Marco Oliviero <s361264@studenti.polito.it>
2026-01-09 11:11:45 +0100

    Adapted main.py to the base structure

--- main.py
+++ main.py
@@ -5,12 +5,10 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from app.models.errors.app_error import AppError
 from app.middleware.error_middleware import error_handler   
-from app.routes import  user_route, auth_route
+from app.routes import  user_route, auth_route, return_route
 from contextlib import asynccontextmanager
 from app.database.database import engine, Base
 from logging import getLogger
-from app.routes.return_route import return_bp
-from app.routes import user_route, auth_route, return_route  # <--- return_route
 
 logger = getLogger(__name__)
 
@@ -36,6 +34,7 @@ app.add_middleware(
 # register routers
 app.include_router(auth_route.router)
 app.include_router(user_route.router)
+app.include_router(return_route.return_bp)
 
 app.add_exception_handler(AppError, error_handler)
 app.add_exception_handler(Exception, error_handler)
@@ -44,13 +43,7 @@ app.add_exception_handler(Exception, error_handler)
 # simple root
 @app.get("/")
 def read_root():
-    return {"message": "FastAPI MVC Demo"}
+    return {"message": "EZShop"}
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=8000)
-
-# جایی که router ها رجیستر میشن:
-app.include_router(auth_route.router)
-app.include_router(user_route.router)
-
-app.include_router(return_bp)
